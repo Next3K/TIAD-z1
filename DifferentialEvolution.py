1bfb0e97102e36c9d57a05c5908ff84a7b3afd13
Pawel <236691@edu.p.lodz.pl>
2023-03-14 09:55:23 +0100

    first implementation of DE

--- DifferentialEvolution.py
+++ DifferentialEvolution.py
@@ -1,12 +1,57 @@
+from random import uniform
+import random
+import math
+
 from Algorithm import Algorithm
 
 
 class DifferentialEvolution(Algorithm):
 
     def find_solution(self, function, min_x: float, max_x: float, min_y: float, max_y: float) -> float:
-        pass
 
-    def __init__(self, x: float, y: float, stop_criterion: str):
+        solution = -math.inf
+        population: [[float]] = [[uniform(min_x, max_x), uniform(min_y, max_y)] in range(self.pop_size)]
+
+        for iteration in range(100):
+            mutants = []
+            # mutation
+            for _ in population:
+                x1, x2, x3 = random.sample(population, 3)
+                v = [(x1[i] + self.F * (x2[i] - x3[i])) for i in range(len(x1))]
+                mutants.append(v)
+
+            # cross-over
+            d = random.randrange(len(mutants[0]))
+            end_pop = [
+                [(mutants[parent_iter][k] if uniform(0, 1) < self.CR or k == d else population[parent_iter][k]) for k in
+                 range(len(population[0]))] for parent_iter in range(len(population))]
+
+            # get new, fitter population
+            new_population = [
+                (end_pop[i] if function(end_pop[i][0], end_pop[i][1]) > function(population[i][0], population[i][1])
+                 else population[i])
+                for i in range(len(population))]
+
+            population = new_population
+
+            # remember current best solution
+            tmp_best_solution = max(list(map(lambda specimen: function(specimen[0], specimen[1]), population)))
+            diff = abs(tmp_best_solution - solution)
+            solution = max(tmp_best_solution, solution)
+
+            # check stop criterion
+            if self.stop_criterion == "iterations" and iteration > 250:
+                break
+            elif self.stop_criterion == "delta" and diff < 0.02:
+                break
+            else:
+                continue
+
+        # get best score
+        return solution
+
+    def __init__(self, stop_criterion: str, F: float = 0.5, pop_size: int = 50, CR: float = 0.5):
         super().__init__(stop_criterion)
-        self.x = x
-        self.y = y
+        self.F = F
+        self.pop_size = pop_size
+        self.CR = CR
